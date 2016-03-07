2748b8b20224a42ca0806b11f8eb406fcf8a013d
Frederic Vogels <frederic.vogels@gmail.com>
2016-03-07 13:34:34 +0100

    Image generation

---
+++ image-generation/generate.rb
@@ -0,0 +1,30 @@
+abort 'Specify data file' unless ARGV.length == 1
+
+load ARGV[0]
+
+
+puts 'Creating disk image'
+`dd if=/dev/zero of=#{IMAGE} bs=#{BLOCK_SIZE} count=#{BLOCK_COUNT}`
+
+
+puts 'Formatting disk image'
+`mkfs.vfat -F 12 -f #{NUMBER_OF_FATS} -n #{VOLUME_NAME} -r #{ROOT_DIR_SIZE} -s #{SECTORS_PER_CLUSTER} #{IMAGE}`
+
+puts 'Mounting'
+`mkdir -p disk`
+`mount -o loop #{IMAGE} disk`
+
+puts 'Adding directories and folders'
+(1..NUMBER_OF_DIRECTORIES).each do |i|
+  `mkdir disk/dir#{i}`
+
+  (1..NUMBER_OF_FILES_PER_DIRECTORY).each do |j|
+    `dd if=/dev/urandom of=disk/dir#{i}/file#{j} count=#{NUMBER_OF_BYTES_PER_FILE} bs=1`
+  end
+end
+
+puts 'Unmounting'
+`umount disk`
+
+puts "Generating xml"
+`java -jar img-reader.jar #{IMAGE} > #{File.basename(IMAGE, '.img')}.xml`
