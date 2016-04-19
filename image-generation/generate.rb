dd4a8f830ea3a4a7761421376f1789e8cfaf8e89
Frederic Vogels <frederic.vogels@gmail.com>
2016-04-19 22:09:11 +0200

    Randomized time/date of files

--- image-generation/generate.rb
+++ image-generation/generate.rb
@@ -3,6 +3,11 @@ abort 'Specify data file' unless ARGV.length == 1
 load ARGV[0]
 
 
+def pad(x)
+  x.to_s.rjust(2, '0')
+end
+
+
 puts 'Creating disk image'
 `dd if=/dev/zero of=#{IMAGE} bs=#{BLOCK_SIZE} count=#{BLOCK_COUNT}`
 
@@ -20,6 +25,14 @@ puts 'Adding directories and folders'
 
   (1..NUMBER_OF_FILES_PER_DIRECTORY).each do |j|
     `dd if=/dev/urandom of=disk/dir#{i}/file#{j} count=#{NUMBER_OF_BYTES_PER_FILE} bs=1`
+    day = rand(27) + 1
+    month = rand(12) + 1
+    year = rand(35) + 1980
+    hour = rand(12)
+    mins = rand(60)
+    secs = rand(60)
+
+    `touch -m -a -t #{year}#{pad month}#{pad day}#{pad hour}#{pad mins}.#{pad secs} disk/dir#{i}/file#{j}`
   end
 end
 
